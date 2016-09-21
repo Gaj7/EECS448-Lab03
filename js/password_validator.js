52e8be3b471c0bebd92a1d91c55a2ab7b7b60623
Grant Jurgensen <g953j882.ku.edu>
2016-09-21 09:37:00 -0500

    finished password validator

--- js/password_validator.js
+++ js/password_validator.js
@@ -1,6 +1,12 @@
 function validate(){
-  var pass1 = document.getElementById("p1").innerHTML;
-  var pass2 = document.getElementById("p2").innerHTML;
+  var pass1 = document.getElementById("p1").value;
+  var pass2 = document.getElementById("p2").value;
 
-  console.log(pass1 + ", " + pass2)
+  if(pass1 != pass2)
+    alert("The passwords entered do not match.")
+  else if(pass1.length < 8)
+      alert("Your password must be at least 8 characters long.")
+  else
+    alert("Password validated.");
+  //console.log(pass1 + ", " + pass2)
 }
